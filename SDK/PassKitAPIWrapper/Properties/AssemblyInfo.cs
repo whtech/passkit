663e2d6eefa8f0bc6ba5c502a8305ea014c1254a
patrick <patrick@passkit.com>
2013-03-27 09:45:47 +0000

    http://www.timeout.com.hk/around-town/features/55081/the-great-escape.html

--- SDK/PassKitAPIWrapper/Properties/AssemblyInfo.cs
+++ SDK/PassKitAPIWrapper/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PassKitAPIWrapper")]
+[assembly: AssemblyTitle("PassKit C# API Wrapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PassKitAPIWrapper")]
+[assembly: AssemblyCompany("PassKit, Inc")]
+[assembly: AssemblyProduct("PassKit C# API Wrapper")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("PassKit and PassK.it are registered trademarks of Anatta Ltd. Passbook and Apple are registered trademarks of Apple Inc.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
---
+++ SDK/readme.txt
@@ -0,0 +1,17 @@
+PassKit CSharp API Wrapper uses:
+
+- RestSharp v104.1.0.0 (http://restsharp.org/)
+ 
+PassKit CSharp API Wrapper - Supported methods
+
+Template methods:
+- List templates;
+- Get passes for template;
+- Update template;
+
+Pass methods:
+- Issue pass;
+- Get pass details (via pass-id);
+- Get pass details (via template & serial);
+- Update pass (via pass-id);
+- Update pass (via template & serial);
