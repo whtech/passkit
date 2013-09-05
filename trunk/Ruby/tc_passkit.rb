42c5ed7b712a4c5d1842b43438e404ead09739f5
ray.lai@hk.passkit.com <ray.lai@hk.passkit.com@402b8cdd-1e4c-0e45-f958-716bb44eb8f1>
2013-09-05 10:35:04 +0000

    Add asserts to properly test json output

--- trunk/Ruby/tc_passkit.rb
+++ trunk/Ruby/tc_passkit.rb
@@ -1,10 +1,19 @@
 require './passkit.rb'
+require 'json'
 require 'test/unit'
 
 class TestPassKit < Test::Unit::TestCase
-  def test_passkit
-    pk = PassKit.new($key, $secret)
-    pk.authenticate
-    pk.template_list
+  def setup
+    @pk = PassKit.new($key, $secret)
+  end
+  def test_authenticate
+    h = JSON.parse @pk.authenticate
+    assert h['success']
+    assert h.has_key?('username')
+  end
+  def test_template_list
+    h = JSON.parse @pk.template_list
+    assert h['success']
+    assert h.has_key?('templates')
   end
 end
