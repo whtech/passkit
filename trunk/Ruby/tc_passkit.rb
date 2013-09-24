3c7e30e7572585f60bf90edeeb3e6b5e759d9e10
ray.lai@hk.passkit.com <ray.lai@hk.passkit.com@402b8cdd-1e4c-0e45-f958-716bb44eb8f1>
2013-09-24 10:34:19 +0000

    Move iOS7 stuff to separate file.

--- trunk/Ruby/tc_passkit.rb
+++ trunk/Ruby/tc_passkit.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'pp'
 require 'test/unit'
 require './passkit.rb'
+require './ts_ios7.rb'
 
 $VERBOSE = true
 
@@ -36,25 +37,6 @@ class TestPassKit < Test::Unit::TestCase
   end
   def test_template_update
     j = JSON.parse @pk.template_update('test', {'textItem' => 'New Defaults'})
-    assert j['success']
-  end
-  def test_pass_update_passid
-    data = {
-      'expirationDate' => '2013-09-17T06:44Z',
-      'voided' => true,
-      'groupingIdentifier' => 'PassKit',
-      'ignoresTimeZone' => true,
-      'associatedStoreIdentifiers' => 12345678,
-      'appLaunchURL' => 'http://passkit.com',
-      'userInfo' => '{"name": "Percy PassKit"}',
-      'beacons' => [{
-	'major' => 123456,
-	'minor' => 654321,
-	'proximityUUID' => 'I AM A UNIQUE ID',
-	'relevantText' => 'irrelevant'
-      }]
-    }
-    j = JSON.parse @pk.pass_update_passid('feKpNoscFpL9', data)
     assert j['success']
   end
 end
